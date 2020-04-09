29947f9ed93fdcd0489396546c4b655eb69066b2
harshasyam2 <122003191@sastra.ac.in>
2020-04-09 11:41:25 +0530

    Create can be possible using a get function

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,4 +8,12 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     render plain: todo.to_pleasant_string
   end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = params[:due_date]
+    completed = params[:completed]
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: completed)
+    render plain: new_todo.to_pleasant_string
+  end
 end
