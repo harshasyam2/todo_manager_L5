eec7925010e6e8e1ca4f79f9ce92ed27a5a2a658
harshasyam2 <122003191@sastra.ac.in>
2020-04-09 12:14:49 +0530

    Use the resources routing convention for Todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,8 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    render plain: Todo.all.map { |todo| todo.to_pleasant_string }
+    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "todos/create", to: "todos#create"
+  #post "todos/create", to: "todos#create"
   resources :todos
 end
