12b0dd76b0e6ad86df27ba8eb2802df2870d4a18
harshasyam2 <122003191@sastra.ac.in>
2020-04-15 11:25:38 +0530

    Combined HTML and CSS with ERB to render todos from Database

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,29 @@
 class Todo < ActiveRecord::Base
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    all.where("due_date<?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date=?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date>?", Date.today)
+  end
+
+  def self.completed
+    all.where("completed=?", true)
+  end
+
+  def self.notcompleted
+    all.where("completed=?", false)
+  end
+
   def to_pleasant_string
-    is_completed = completed ? "[x]" : "[]"
-    "#{id}.  #{todo_text}  #{due_date}  #{is_completed}"
+    "#{id} #{completed}"
   end
 end
--- db/migrate/20200409095855_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users do |t|
-      t.text :name
-      t.string :email
-      t.string :password
-    end
-  end
-end
