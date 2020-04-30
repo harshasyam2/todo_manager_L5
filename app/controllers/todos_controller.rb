1205e0c42e92b0aa3acc0009a00f950f48dcca60
harshasyam2 <122003191@sastra.ac.in>
2020-04-30 17:03:35 +0530

    input field tags are added to the form elements

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -25,6 +25,13 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "Hey! you updated the complete status to #{completed}"
+    redirect_to todos_path
+  end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.destroy
+    redirect_to todos_path
   end
 end
