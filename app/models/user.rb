faff4a5029edee2ac51debd2a70606e433554b3b
harshasyam2 <122003191@sastra.ac.in>
2020-05-07 16:34:10 +0530

    AR validations are added to todos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :email, presence: true
-  validates :email, presence: true
+  validates :password, presence: true
   has_many :todos
   has_secure_password
 
